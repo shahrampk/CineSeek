5773c0668746ea926af1c1ec8a06ffa7628a91a8
Muhammad Shahram <mshahram574@gmail.com>
2025-09-27 23:11:25 +0500

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        navbar: "#1a1e21",
+      },
+    },
   },
   plugins: [
     function ({ addUtilities }) {
