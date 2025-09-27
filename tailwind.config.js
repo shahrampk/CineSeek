2833f5b94fa89111f508a235f44970374d0f9ca8
Muhammad Shahram <mshahram574@gmail.com>
2025-09-28 02:39:13 +0500

    provided feature section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        "3xl": "1920px",
+      },
+      maxWidth: {
+        slider: "1420px",
+      },
       backgroundColor: {
         navbar: "#1a1e21",
+        "search-bar": "#343a40",
+      },
+      backgroundImage: {
+        "linear-gradient":
+          "linear-gradient(to right bottom, #15181a 40%, #1d2021)",
       },
     },
   },
