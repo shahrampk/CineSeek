0b0a5030462646b0f247ece85dfacb0cb0287fa0
Muhammad Shahram <mshahram574@gmail.com>
2025-10-07 20:51:02 +0500

    making files ready to publish

--- public/js/Views/watchList-view.js
+++ public/js/Views/watchList-view.js
@@ -2,26 +2,31 @@ import View from "./View.js";
 class WatchListView extends View {
   _parentEL = document.querySelector("#watchlist");
   _generateMarkUp() {
+    console.log(this._data);
+
     if (this._data.length <= 0) {
       return `
         <li class="group flex items-center justify-center gap-2 px-5 py-10 
                rounded-md border border-transparent 
                text-gray-300 md:text-lg font-medium hover:bg-search-bar hover:text-white
                transition-all duration-300 ease-in-out">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                      stroke="currentColor" class="w-12 h-12 mr-1">
-                      <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
-                  </svg>
-               <p>No Movie yet. Find a nice movie and add it into Watchlist :)</p>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-12 h-12 mr-1">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+                </svg>
+                <p>No Movie yet. Find a nice movie and add it into Watchlist :)</p>
         </li>
       `;
     }
     return this._data
       .map(
-        (movieDate, i) =>
-          `
+        (movieDate, i) => `
         <li class="group">
+           <a target="_blank" href="https://www.youtube.com/watch?v=${
+             movieDate.trailerKey
+           }" >
+               
             <div 
               class="flex items-center justify-between p-4 rounded-xl 
                     backdrop-blur hover:border-red-500 hover:-translate-y-1 hover:bg-search-bar hover:shadow-lg
@@ -63,13 +68,13 @@ class WatchListView extends View {
                 </svg>
               </button>
             </div>
+          </a>
 </li>
 `
       )
       .join("");
   }
   loadWatchListMovie(data) {
-
     window.addEventListener("load", () => {
       this.render(data);
     });
